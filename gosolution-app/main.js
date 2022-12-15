77d1bf58449870df91ed1e14583259e7b3194590
Max25832 <115776441+Max25832@users.noreply.github.com>
2022-12-15 17:28:04 +0100

    changeing the Map back to OSM

--- gosolution-app/main.js
+++ gosolution-app/main.js
@@ -1,6 +1,7 @@
 //replaced the map display in main.js with the new OGC API 
 //imports for Map layer
 import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
 import OGCMapTile from 'ol/source/OGCMapTile';
 import TileLayer from 'ol/layer/Tile';
 //imports for Vector Layers
@@ -11,12 +12,12 @@ import View from 'ol/View';
 
 const map = new Map({
   target: 'map',
+
   //creating the Layers
   layers: [
     new TileLayer({
-      source: new OGCMapTile({
-        url: 'https://maps.ecere.com/ogcapi/collections/blueMarble/map/tiles/WebMercatorQuad',
-      }),
+      source: //new OGCMapTile({
+        new OSM()
     }),
     new VectorTileLayer({
       source: new OGCVectorTile({
